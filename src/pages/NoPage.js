3395efbf0d69d5f235980e5c76c14a73230d5385
Shubham <shubhamkumar7112002@gmail.com>
2024-05-31 20:22:15 +0530

    404 page modifed

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import img from '../assets/404.png'
 
 function NoPage() {
   return (
-    <div>NoPage</div>
+    <div className='w-full h-screen flex items-center justify-center bg-yellow-50'> 
+      <img src={img} alt="page not found" width="400px"/>
+    </div>
   )
 }
 
